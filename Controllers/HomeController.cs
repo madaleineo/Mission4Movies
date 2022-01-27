b2a2a8a46d504985a88425cca3ac4100767842c0
madaleineo <90480511+madaleineo@users.noreply.github.com>
2022-01-26 23:50:40 -0700

    last one

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using FilmCollectionMission4.Models;
+
+namespace FilmCollectionMission4.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private MovieInfo blahContext { get; set; }
+
+        public HomeController(ILogger<HomeController> logger, MovieInfo someName)
+        {
+            _logger = logger;
+            blahContext = someName;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Movies()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Movies(ApplicationResponse ar)
+        {
+            blahContext.Add(ar);
+            blahContext.SaveChanges();
+
+            return View("Confirmation", ar);
+        }
+
+        public IActionResult MyPodcasts()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
