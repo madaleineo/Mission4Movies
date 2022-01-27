53295b5985a86f969d00f3a7269059bc01c4b6ce
madaleineo <90480511+madaleineo@users.noreply.github.com>
2022-01-26 23:48:04 -0700

    Initial commit

---
+++ Migrations/20220127063601_Initial.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FilmCollectionMission4.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Responses",
+                columns: table => new
+                {
+                    MovieID = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Category = table.Column<string>(nullable: false),
+                    Title = table.Column<string>(nullable: false),
+                    Year = table.Column<int>(nullable: false),
+                    DirectorFirst = table.Column<string>(nullable: false),
+                    DirectorLast = table.Column<string>(nullable: false),
+                    Rating = table.Column<string>(nullable: false),
+                    Edited = table.Column<bool>(nullable: false),
+                    LentTo = table.Column<string>(nullable: true),
+                    Notes = table.Column<string>(maxLength: 25, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Responses", x => x.MovieID);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Responses",
+                columns: new[] { "MovieID", "Category", "DirectorFirst", "DirectorLast", "Edited", "LentTo", "Notes", "Rating", "Title", "Year" },
+                values: new object[] { 1, "Action/Adventure", "Joe", "Johnston", false, "", "", "PG-13", "Captain America: The First Avenger", 2011 });
+
+            migrationBuilder.InsertData(
+                table: "Responses",
+                columns: new[] { "MovieID", "Category", "DirectorFirst", "DirectorLast", "Edited", "LentTo", "Notes", "Rating", "Title", "Year" },
+                values: new object[] { 2, "Action/Adventure", "Joe", "Russo", false, "", "", "PG-13", "Captain America: Civil War", 2016 });
+
+            migrationBuilder.InsertData(
+                table: "Responses",
+                columns: new[] { "MovieID", "Category", "DirectorFirst", "DirectorLast", "Edited", "LentTo", "Notes", "Rating", "Title", "Year" },
+                values: new object[] { 3, "Action/Adventure", "Joe", "Russo", false, "", "", "PG-13", "Captain America: Winter Soldier", 2014 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Responses");
+        }
+    }
+}
---
+++ Models/MovieInfo.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace FilmCollectionMission4.Models
+{
+    public class MovieInfo : DbContext
+    {
+        public MovieInfo (DbContextOptions<MovieInfo> options) : base (options)
+        {
+            //leave blank for now
+        }
+
+        public DbSet<ApplicationResponse> Responses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            mb.Entity<ApplicationResponse>().HasData(
+
+                new ApplicationResponse
+                {
+                    MovieID = 1,
+                    Category = "Action/Adventure",
+                    Title = "Captain America: The First Avenger",
+                    Year = 2011,
+                    DirectorFirst = "Joe",
+                    DirectorLast = "Johnston",
+                    Rating = "PG-13",
+                    Edited = false,
+                    LentTo = "",
+                    Notes = "",
+                },
+                new ApplicationResponse
+                {
+                    MovieID = 2,
+                    Category = "Action/Adventure",
+                    Title = "Captain America: Civil War",
+                    Year = 2016,
+                    DirectorFirst = "Joe",
+                    DirectorLast = "Russo",
+                    Rating = "PG-13",
+                    Edited = false,
+                    LentTo = "",
+                    Notes = "",
+                },
+                new ApplicationResponse
+                {
+                    MovieID = 3,
+                    Category = "Action/Adventure",
+                    Title = "Captain America: Winter Soldier",
+                    Year = 2014,
+                    DirectorFirst = "Joe",
+                    DirectorLast = "Russo",
+                    Rating = "PG-13",
+                    Edited = false,
+                    LentTo = "",
+                    Notes = "",
+                }
+            );
+        }
+    }
+}
